0ee390650761738b24e291e32c9599c9c568dd5a
GamerCleanVic <jonassd2000@gmail.com>
2023-10-24 01:03:15 -0500

    cssNaUnha

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
-<?php
-    //const
-    define("URL", "https://twitter.com");
+<!DOCTYPE html>
+<html lang="en, pt_br, es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css" /> -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <script type="text/javascript" src="assets/js/jquery.min.js"><script/>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"><script/>
+    <script type="text/javascript" src="assets/js/script.js"><script/> -->
+    <title>CLASSIFICADOS</title>
+</head>
+<body>
+    <nav class="
+        navbar-main
+    ">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="./" class="navbar-brand-a">Classificados</a>
+            </div>
+            <div class="
+                nav
+                navba-bar
+                navbar-right
+            ">
 
-    echo 
-        "O site é: <a 
-            href='https://twitter.com' target='_blank'
-        >".URL."</a>"
-    ;
-?>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
